a41843dddb705604be0faac30d7a6d0c4a17d16d
Saurav Adhikari <sauravads123@gmail.com>
2021-05-03 19:10:20 +0545

    chore : test

--- packages/admin/src/App.js
+++ packages/admin/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Admin is up and running
+        Admin is up and running - Test
       </header>
     </div>
   );
--- packages/user/src/App.js
+++ packages/user/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        User is up and running
+        User is up and running - Test
       </header>
     </div>
   );
