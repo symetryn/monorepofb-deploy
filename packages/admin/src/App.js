947b4e14d89dcb7438a95618c60263bc1dca9e70
Rojan <roseonmhr@gmail.com>
2021-07-07 17:02:37 +0545

    updated user/admin text

--- packages/admin/src/App.js
+++ packages/admin/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Admin is up and running - Test
+        Hello from admin side
       </header>
     </div>
   );
--- packages/user/src/App.js
+++ packages/user/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        User is up and running - Test
+        Hello from user side
       </header>
     </div>
   );
